d7d72664371b9f251b0b4c35b7245f9a7aec1559
saiyan3.14 <141233559+SaiyanPi@users.noreply.github.com>
2025-01-12 16:16:44 +0545

    completed code-coverage

--- 10. Unit Testing/repository pattern/InvoiceApp.UnitTest2/obj/Debug/net8.0/InvoiceApp.UnitTest2.AssemblyInfo.cs
+++ 10. Unit Testing/repository pattern/InvoiceApp.UnitTest2/obj/Debug/net8.0/InvoiceApp.UnitTest2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvoiceApp.UnitTest2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d746ef5452662a24e60a9051db693899680849c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e715025e3ad1e5c47d987b29595fd563f9bf10e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvoiceApp.UnitTest2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvoiceApp.UnitTest2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
