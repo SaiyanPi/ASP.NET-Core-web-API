6372634b97b99798cdb2dc713d7fc4ef0e1673c3
saiyan3.14 <141233559+SaiyanPi@users.noreply.github.com>
2025-03-17 08:41:55 +0545

    added completed GraphQL

--- 14. Getting Started with GraphQL/End/SchoolManagement/obj/Debug/net8.0/SchoolManagement.AssemblyInfo.cs
+++ 14. Getting Started with GraphQL/End/SchoolManagement/obj/Debug/net8.0/SchoolManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e4d4ed8a1fb195c1cc0cc98a6cef604338f82c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac77df5fe026b47eefc8c00c7ae462c0c2a85ce5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
