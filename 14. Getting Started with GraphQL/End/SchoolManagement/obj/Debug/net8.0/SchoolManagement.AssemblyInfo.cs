7f7df4740afd4245cd6adb142cb7f614c6f3d7a7
saiyan3.14 <141233559+SaiyanPi@users.noreply.github.com>
2025-03-23 13:40:12 +0545

    commit for going back on GraphQL mutation

--- 14. Getting Started with GraphQL/End/SchoolManagement/obj/Debug/net8.0/SchoolManagement.AssemblyInfo.cs
+++ 14. Getting Started with GraphQL/End/SchoolManagement/obj/Debug/net8.0/SchoolManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac77df5fe026b47eefc8c00c7ae462c0c2a85ce5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+190f708c35904f821a9694199782a2d64a0199a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
