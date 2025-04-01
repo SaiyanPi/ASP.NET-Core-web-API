620c6627659a17d8a87ae217629fe3f4a077efce
saiyan3.14 <141233559+SaiyanPi@users.noreply.github.com>
2025-04-01 16:33:20 +0545

    authentication- login endpoint added

---
+++ 15. Getting Started with SignalR/v2/ChatApp.Server/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.Server.Hubs;
+
+[Authorize]
+public class ChatHub : Hub
+{
+    public Task SendMessage(string username, string message)
+    {
+        return Clients.All.SendAsync("ReceiveMessage", username, message);
+    }
+}
+
+
