c55b66ae6666df95ec1503fd72e933e5c2a304bc
saiyan3.14 <141233559+SaiyanPi@users.noreply.github.com>
2025-04-03 20:14:41 +0545

    authentication- blazor authentication done, messages displayed from both clients

--- 15. Getting Started with SignalR/v2/ChatApp.Server/obj/Debug/net8.0/ChatApp.Server.AssemblyInfo.cs
+++ 15. Getting Started with SignalR/v2/ChatApp.Server/obj/Debug/net8.0/ChatApp.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatApp.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+620c6627659a17d8a87ae217629fe3f4a077efce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fc440d13787a848a548a05202201a167c9a1fff")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatApp.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatApp.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
