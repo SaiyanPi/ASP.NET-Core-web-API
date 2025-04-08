ac5f5f0ba944bf294bd004c44bce6fed8489c5cf
saiyan3.14 <141233559+SaiyanPi@users.noreply.github.com>
2025-04-08 16:15:36 +0545

    using strongly typed hub done

--- 15. Getting Started with SignalR/v3/ChatApp.Server/Hubs/ChatHub.cs
+++ 15. Getting Started with SignalR/v3/ChatApp.Server/Hubs/ChatHub.cs
@@ -5,31 +5,49 @@ using Microsoft.AspNetCore.SignalR;
 namespace ChatApp.Server.Hubs;
 
 [Authorize]
-public class ChatHub : Hub
+public class ChatHub : Hub<IChatClient>
 {
-    public Task SendMessage(string username, string message)
+    // public Task SendMessage(string username, string message)
+    // {
+    //     return Clients.All.SendAsync("ReceiveMessage", username, message);
+    // }
+    public Task SendMessage(string user, string message)
     {
-        return Clients.All.SendAsync("ReceiveMessage", username, message);
+        return Clients.All.ReceiveMessage(user, message);
+    }
+
+    // send message to a specified user
+    // public Task SendMessageToUser(string user, string toUser, string message)
+    // {
+    //     return Clients.User(toUser).SendAsync("ReceiveMessage", user, message);
+    // }
+    public Task SendMessageToUser(string user, string toUser, string message)
+    {
+        return Clients.User(toUser).ReceiveMessage(user, message);
     }
 
     // user connected event
+    // public override async Task OnConnectedAsync()
+    // {
+    //     await Clients.All.SendAsync("UserConnected", Context.User.Identity.Name);
+    //     await base.OnConnectedAsync();
+    // }
     public override async Task OnConnectedAsync()
     {
-        await Clients.All.SendAsync("UserConnected", Context.User.Identity.Name);
+        await Clients.All.UserConnected(Context.User.Identity.Name);
         await base.OnConnectedAsync();
     }
 
     // user discoinnected event
+    // public override async Task OnDisconnectedAsync(Exception? exception)
+    // {
+    //     await Clients.All.SendAsync("UserDisconnected", Context.User.Identity.Name);
+    //     await base.OnDisconnectedAsync(exception);
+    // } 
     public override async Task OnDisconnectedAsync(Exception? exception)
     {
-        await Clients.All.SendAsync("UserDisconnected", Context.User.Identity.Name);
+        await Clients.All.UserDisconnected(Context.User.Identity.Name);
         await base.OnDisconnectedAsync(exception);
-    } 
-
-    // send message to a specified user
-    public Task SendMessageToUser(string user, string toUser, string message)
-    {
-        return Clients.User(toUser).SendAsync("ReceiveMessage", user, message);
     }
 
 }
