3af07d49ff64ec2c2164ed9b586437ec5d340675
saiyan3.14 <141233559+SaiyanPi@users.noreply.github.com>
2025-05-23 12:15:25 +0545

    final version of source code(FavoriteItenV2) added for distributed cache

--- 17. ASP.NET Core Web API Common Practices/FavoriteItemV2/Program.cs
+++ 17. ASP.NET Core Web API Common Practices/FavoriteItemV2/Program.cs
@@ -1,8 +1,45 @@
+using System.Text;
+using FavoriteItemV2.Authentication;
+using FavoriteItemV2.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ApplicationDbContext>();
+builder.Services.AddIdentityCore<AppUser>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    var secret = builder.Configuration["JwtConfig:Secret"];
+    var issuer = builder.Configuration["JwtConfig:ValidIssuer"];
+    var audience = builder.Configuration["JwtConfig:ValidAudiences"];
+    if (secret is null || issuer is null || audience is null)
+    {
+        throw new ApplicationException("Jwt is not set in theconfiguration");
+    }
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = audience,
+        ValidIssuer = issuer,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret))
+    };
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,6 +54,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
--- 17. ASP.NET Core Web API Common Practices/FavouriteItemApi/obj/Debug/net8.0/FavouriteItemApi.AssemblyInfo.cs
+++ 17. ASP.NET Core Web API Common Practices/FavouriteItemApi/obj/Debug/net8.0/FavouriteItemApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FavouriteItemApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84fc5a4308557458736f1c89ed9e024b60e87d8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f0292371244efc943fb57cd35b53abd712f220d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FavouriteItemApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FavouriteItemApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
