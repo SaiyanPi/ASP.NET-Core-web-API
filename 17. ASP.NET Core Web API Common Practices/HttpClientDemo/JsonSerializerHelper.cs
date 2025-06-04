d27c214932264d737c17da6a3927b724f94c158d
saiyan3.14 <141233559+SaiyanPi@users.noreply.github.com>
2025-06-04 16:25:37 +0545

    IHttpClientFactory interface to manage HttpClient instances - completed

---
+++ 17. ASP.NET Core Web API Common Practices/HttpClientDemo/JsonSerializerHelper.cs
@@ -0,0 +1,28 @@
+using System.Text.Json;
+
+namespace HttpClientDemo;
+
+public static class JsonSerializerHelper
+{
+    public static string SerializeWithCamelCase<T>(T value)
+    {
+        var options = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            DictionaryKeyPolicy = JsonNamingPolicy.CamelCase,
+        };
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    public static T? DeserializeWithCamelCase<T>(string json)
+    {
+        var options = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            DictionaryKeyPolicy = JsonNamingPolicy.CamelCase,
+        };
+
+        return JsonSerializer.Deserialize<T>(json, options);
+    }
+}
---
+++ 17. ASP.NET Core Web API Common Practices/HttpClientDemo/Models/Company.cs
@@ -0,0 +1,7 @@
+namespace HttpClientDemo.Models;
+public class Company
+{
+    public string Name { get; set; } = string.Empty;
+    public string CatchPhrase { get; set; } = string.Empty;
+    public string Bs { get; set; } = string.Empty;
+}
